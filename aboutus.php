fd8dd1c77c160ddc51da48a6633da082f5d9e717
samsontg7 <samsondereje7@gmail.com>
2024-06-11 21:18:56 +0200

    about us

--- aboutus.php
+++ aboutus.php
@@ -22,7 +22,7 @@
           <h2 class="search">About Us</h2>
              <div class="div2">
              <h3>Student Result Management System</h2>
-             <p>This Software Application unbelievably unravels and quickens the result management system with unique templates by providing the administration a secure database system for storing, evaluating and publishing the test scores and grades of candidates online. The database likewise allows the student to observe and gander at the exam results on the web at whatever point necessary.</p>
+             <p>This Software Application quickens the result management system by providing the an administration and secure database system for storing, evaluating and publishing the test scores and grades of candidates online. The database likewise allows the student to observe the exam results on the web when necessary.</p>
              <p> Student Result Management System is a technological opportunity for the school, college university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results  evaluation, reporting and distribution.</p>
              </div>  
           </div>
